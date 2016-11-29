d74c44150ce7a6af91cda4a6fccf15c35c6c13f3
Subham Dey <subhamdey@outlook.com>
2016-11-29 16:21:41 +0530

    added routes and views

--- routes.js
+++ routes.js
@@ -4,12 +4,41 @@ module.exports = function(app,io){
     res.render('home');
   });
 
-  app.post('/chat', function(req, res) {
-    var username = req.body.username;
+  var users = [];
+  var rooms = [];
 
-    res.render('chat', {user: username});
+  app.get('/rooms', function(req, res) {
+    //console.log(io.sockets.adapter.rooms);
+    res.render('rooms', {user: "req.session.username", rooms: io.sockets.adapter.rooms});
+  })
+
+  app.post('/rooms', function(req, res) {
+    var username = req.body.username;
+    users.push(username);
+
+    res.render('rooms', {user: username, rooms: io.sockets.adapter.rooms});
   });
 
+  app.post('/create', function(req, res) {
+    var room = req.body.newroom;
 
+    res.redirect('/chat/' + room);
+
+  });
+
+  app.get('/chat/:id', function(req, res) {
+    var room = req.params.id;
+    console.log('room= ' + room);
+    res.render('chat', {room: room});
+  });
+
+  io.on('connection', function(socket) {
+
+
+    socket.on('new-message', function(data) {
+      io.to('').emit('showmsg', data);
+    });
+
+  });
 
 };
